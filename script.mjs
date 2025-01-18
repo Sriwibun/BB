e59e833cdea7f3c7c3293d190ea0b78114c6552c
sriwibun <sriwiw20@gmail.com>
2025-01-18 05:16:41 +0100

    made a route for life saving quotes

--- script.mjs
+++ script.mjs
@@ -11,13 +11,30 @@ function getRoot(req, res, next) {
     res.status(HTTP_CODES.SUCCESS.OK).send('Hello World').end();
 }
 
-server.get(`/tmp/poem`,(req, res) => {
-    res.send(`
+server.get(`/tmp/poem`,(req, res,) => {
+    res.status(HTTP_CODES.SUCCESS.OK).send(`
     Roser er røde,<br>
     fioler er er blå,<br>
     jeg er på ferie,<br>
     det er ikke du...`
-    );
+    ).end();
+});
+
+const quotes = [
+    `To be or not to be...`,
+    `Dont think, just do it...`,	
+    `Actions speak louder than speaker`,
+    `You are who are you.`,
+    `It is what is it.`,
+    `Dont chase, attack.`,
+    `People come, clean as you go.`,
+    `Take the risk, keep the change.`
+];
+
+server.get('/tmp/quote', (req, res) => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    const randomQuote = quotes[randomIndex];
+    res.status(HTTP_CODES.SUCCESS.OK).send(randomQuote).end();
 });
 
 server.get("/", getRoot);
