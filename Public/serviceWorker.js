ef93afb470a56fbf151186635c6e85edcf9c4d60
sriwibun <sriwiw20@gmail.com>
2025-03-02 22:23:52 +0100

    some changes...

--- Public/serviceWorker.js
+++ Public/serviceWorker.js
@@ -1,4 +1,4 @@
 // SELF => service worker itself
-// waitUntil => wait until the promise is resolved (fancy async await
+// waitUntil => wait until the promise is resolved (fancy async await)
 // contentToCache => array of files to cache ex. API, index.html, app.mjs, style.css, icons, etc.
 // cacheID => name of the list of cache elements to be stored
