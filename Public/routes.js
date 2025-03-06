d66e36cd7fce9092cd3af159f13e251006f32fb1
sriwibun <sriwiw20@gmail.com>
2025-03-06 21:43:34 +0100

    remade deleted files...home.js, add.js...

--- Public/routes.js
+++ Public/routes.js
@@ -4,4 +4,16 @@ const routes = {
     "/remove":"/public/templates/remove.html",
 };
 
+async function navigateTo(event) {
+    if (event) event.preventDefault();
+    const path = window.location.pathname;
+    const route = routes[path] || routes ["/"];
 
+    const content = await fetch(route).then(res => res.text());
+    document.getElementById("app").innerHTML = content;
+}
+
+window.ompopstate = navigateTo;
+document.addEventListener("DOMContentLoaded", navigateTo);
+export { navigateTo };
+// Compare this snippet from Public/templates/home.html:    
