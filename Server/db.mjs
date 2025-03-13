8a42b3ec625c3605a6538775f78ccf17991cb216
sriwibun <sriwiw20@gmail.com>
2025-03-13 03:30:31 +0100

    trying to undig...

--- Server/db.mjs
+++ Server/db.mjs
@@ -1,14 +1,16 @@
-import { Pool } from "pg";
+import pkg from 'pg';
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const { Pool } = pkg;
 
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT || 8000,
+    port: process.env.DB_PORT || 5432,
 });
 
 export default pool;
