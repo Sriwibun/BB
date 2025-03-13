27588baa824104533ddc14cdd0c915ed31628679
sriwibun <sriwiw20@gmail.com>
2025-03-13 19:19:08 +0100

    Revert "trying to undig..."
    This reverts commit 8a42b3ec625c3605a6538775f78ccf17991cb216.

--- Server/db.mjs
+++ Server/db.mjs
@@ -1,16 +1,14 @@
-import pkg from 'pg';
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
-
-const { Pool } = pkg;
 
 const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT || 5432,
+    port: process.env.DB_PORT || 8000,
 });
 
 export default pool;
