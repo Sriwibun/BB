caeb0d97a86bf79a48f2dc6f801d323c4d049016
sriwibun <sriwiw20@gmail.com>
2025-03-14 13:50:03 +0100

    undo changes

--- server/db.mjs
+++ server/db.mjs
@@ -5,16 +5,15 @@ dotenv.config();
 
 const { Pool } = pkg;
 const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
     port: process.env.DB_PORT,
-    connectionString: process.env.DATABASE_URL,
-    ssl: process.env.DATABASE_URL.includes("render.com")
-    ? { rejectUnauthorized: false }  // Render requires this
-    : false
-    });
-);
+});
 
 export default pool;
